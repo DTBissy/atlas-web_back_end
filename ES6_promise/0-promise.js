11552f696e70aac13c90c9d29d52761f72718e3d
Dtbissy <isaactheedwards@gmail.com>
2024-10-22 14:13:15 -0500

    Finally some headway

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,3 +1,7 @@
-function getResponseFromAPI() {
-  return Promise
-}
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Success');
+    }, 1000);
+  });
+};
