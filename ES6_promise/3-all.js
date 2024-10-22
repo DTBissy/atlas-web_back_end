6ac3dfc410b65f7cb0d076f937179e8bd21b529e
Dtbissy <isaactheedwards@gmail.com>
2024-10-22 17:09:27 -0500

    That message is gonna come in clutch

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,6 +3,15 @@ import { uploadPhoto } from "./util"
 
 export default function handleProfileSignup() {
   return Promise.all([createUser(), uploadPhoto()])
-    .then(() => console.log('body firstname lastName'))
-    .catch(new Error('Signup system offline'));
+    /** The results const assigns the returns of createUser and uploadPhoto
+     * by the order asked to be received in below. Making the data avalibale in both accessible
+     * in the console.log.
+     */
+    .then(results => {
+      const [user, photo] = results;
+      console.log(photo.body, user.firstName, user.lastName)
+    })
+    .catch(error => {
+      console.error("Signup system offline");
+    });
 }
