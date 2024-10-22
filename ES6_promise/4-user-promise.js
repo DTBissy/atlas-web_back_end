89e86ca4439cda6671dacef723f656ff691c69b8
Dtbissy <isaactheedwards@gmail.com>
2024-10-22 18:52:03 -0500

    Making money

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,6 +1,10 @@
 
 export default function signUpUser(firstName, lastName) {
-  return Promise((resolve) => {
-    resolve({ firstName }, { lastName });
+  const names = new Promise((resolve) => {
+    resolve(
+      { firstName, lastName },
+    );
   });
+  return names;
 }
+
