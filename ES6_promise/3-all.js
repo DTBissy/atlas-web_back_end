34d54f718c16204b2640bc0a133e5dcdd7368f73
Dtbissy <isaactheedwards@gmail.com>
2024-10-22 17:43:11 -0500

    Forgot to append results

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,16 +2,12 @@ import { createUser, uploadPhoto } from "./util"
 
 export default function handleProfileSignup() {
   return Promise.all([createUser(), uploadPhoto()])
-    /** The results const assigns the returns of createUser and uploadPhoto
-     * by the order asked to be received in below. Making the data avalibale in both accessible
-     * in the console.log.
-     */
     .then((results) => {
       let body = results.body;
       createUser()
         .then((results) => {
-          let firstName = firstName;
-          let lastName = lastName;
+          let firstName = results.firstName;
+          let lastName = results.lastName;
           console.log(`${body} ${firstName} ${lastName}`);
         })
         .catch(() => console.log('Signup system offline'));
