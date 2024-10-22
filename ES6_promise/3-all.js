e991ede9976360dbbefbc4e87650c69e2ef6f567
Dtbissy <isaactheedwards@gmail.com>
2024-10-22 17:56:53 -0500

    LEts see how right i wwas an hour or 2 ago

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,15 +1,17 @@
-import { createUser, uploadPhoto } from "./utils"
+import { createUser } from "./utils"
+import { uploadPhoto } from "./utils"
 
 export default function handleProfileSignup() {
-  return (uploadPhoto())
-    .then((results) => {
-      let photo = results.body
-      createUser()
-        .then((results) => {
-          let firstName = results.firstName;
-          let lastName = results.lastName;
-          console.log(`${photo} ${firstName} ${lastName}`);
-        })
-        .catch(() => console.log('Signup system offline'));
+  return Promise.all([createUser(), uploadPhoto()])
+    /** The results const assigns the returns of createUser and uploadPhoto
+     * by the order asked to be received in below. Making the data avalibale in both accessible
+     * in the console.log.
+     */
+    .then(results => {
+      const [user, photo] = results;
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
+    .catch(error => {
+      console.error("Signup system offline");
+    });
 }
