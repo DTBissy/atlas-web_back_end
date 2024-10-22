6ea22b1f405c1f4b76d946962b2f7034163fa9b8
Dtbissy <isaactheedwards@gmail.com>
2024-10-22 15:34:56 -0500

    Maye like this

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
-    if (promise) Promise.resolve({ status: 200, body: 'success' }, console.log('Got Response'));
-    else reject(Error('Hm like this')).then(resolve, reject);
+    if (promise) resolve({ status: 200, body: 'success' }, console.log('Got a response from the API'));
+    else if (promise !== true) reject(new Error(), console.log('Got a response from the API'))
   });
 }
