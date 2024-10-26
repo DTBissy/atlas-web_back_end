76b626944b47a57734e4e32042c8c8465f85d4cc
Dtbissy <isaactheedwards@gmail.com>
2024-10-26 16:39:24 -0500

    Done task 1 thriugh 8

---
+++ ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,9 @@
+export default function getBudgetObject(income = 400, gdp = 700, capita = 900) {
+  const budget = {
+    income: income,
+    gdp: gdp,
+    capita: capita,
+  };
+
+  return budget;
+}
