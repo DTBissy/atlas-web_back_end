bba41b6c63c82d7be318eb41b99d7af7527ce3d9
Dtbissy <isaactheedwards@gmail.com>
2024-10-30 20:34:27 -0500

    Hopefully fixed number 2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -4,38 +4,42 @@ export default class HolbertonCourse {
     this._length = length;
     this._students = students;
 
-    Object.defineProperty(this, 'name', {
-      get: () => this._name,
-      set: (value) => {
-        if (typeof value === 'string') {
-          return this._name = value;
-        } else {
-          throw new Error('Name must be a string')
-        }
-      }
-    });
+  }
 
-    Object.defineProperty(this, 'length', {
-      get: () => this._length,
-      set: (value) => {
-        if (typeof value === 'number') {
-          return this._length;
-        } else {
-          throw new Error('Length must be a number');
-        }
-      }
-    });
+  get name() {
+    this._name;
+  }
 
-    Object.defineProperty(this, 'students', {
-      get: () => this.__students,
-      set: (value) => {
-        if (Array.isArray(value)) {
-          return this._students = value;
-        } else {
-          throw new Error('Students must be an array')
-        }
-      }
-    });
+  get length() {
+    this._length;
+  }
+
+  get students() {
+    this._students;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name;
+    } else {
+      throw new Error('Name must be a string');
+    }
+  }
+
+  set length(value) {
+    if (typeof value === 'number') {
+      this._length;
+    } else {
+      throw new Error('Length must be a number');
+    }
+  }
+
+  set students(value) {
+    if (!Array.isArray(value) && value.some((student) => typeof student !== 'string')) {
+      this._students;
+    } else {
+      throw new Error('Students must be an array with strings')
+    }
   }
 }
 
