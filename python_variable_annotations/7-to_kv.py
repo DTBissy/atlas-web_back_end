44fd7e4e960a3f4921914dd5bdab5ee1da2a959c
Dtbissy <isaactheedwards@gmail.com>
2025-01-07 14:06:37 -0600

    Like so

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,6 +3,6 @@
 from typing import List, Optional, Union, Tuple
 
 
-def to_kv(k:str, v: Union[int | float] )-> Tuple[str, float]:
+def to_kv(k:str, v: Union[int, float] )-> Tuple[str, float]:
     """Returns data into a tuple """
     return (k, v * v)
