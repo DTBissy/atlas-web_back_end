f05b9af6dde4af6d5ce0b2c082bde960a73e8fd9
Dtbissy <isaactheedwards@gmail.com>
2025-01-07 13:12:12 -0600

    Task 1 and Task 2 completed

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+import math
+"""Uses math library for floor calculation"""
+
+
+def floor(n: float):
+    """Returns the lowest rounded num of n """
+    return math.floor(n)
