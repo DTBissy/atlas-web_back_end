0b566fd24fbc2c4f9f06c420a7d37e2e435c9af7
Dtbissy <isaactheedwards@gmail.com>
2025-01-07 13:02:53 -0600

    Docstring Documentations are the way to go

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """A module for python variable annotations"""
 
+
 def add(a: float, b: float) -> float:
-   """A simple add function to add float a and float b together and return a float"""
-   return a + b
+    """A simple add function to add float a and float b together and return a float"""
+    return a + b
