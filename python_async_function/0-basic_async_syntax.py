4698426bfb1eae6360bb132bb0b1b580874a5e91
Dtbissy <isaactheedwards@gmail.com>
2025-01-07 20:46:46 -0600

    Did it

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+"""Will display a asynchronous function max delay"""
+import asyncio
+import random
+from typing import *
+
+
+async def wait_random(max_delay: Union[int, float] = 10) :
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+async def main():
+    print("Running wait random functions")
+    await wait_random()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
