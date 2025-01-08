e07092b0152493fbbda25484b4be12d5cfcbfd96
Dtbissy <isaactheedwards@gmail.com>
2025-01-07 20:57:01 -0600

    Like I love when this happens. I write code that works but doesnt pass the checker. And the checker wont tell me why

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import random
 from typing import *
 
 
-async def wait_random(max_delay: float = 10) -> float :
+async def wait_random(max_delay: int = 10) -> float :
     """Uses async to find to delay"""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
