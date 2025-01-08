d49e66da3620f5f2eb0f84870404bcefb51ad1d7
Dtbissy <isaactheedwards@gmail.com>
2025-01-07 20:55:34 -0600

    More stripping

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -5,8 +5,8 @@ import random
 from typing import *
 
 
-async def wait_random(max_delay: Union[int, float] = 10) -> float :
+async def wait_random(max_delay: float = 10) -> float :
     """Uses async to find to delay"""
     delay = random.uniform(0, max_delay)
-    await delay
+    await asyncio.sleep(delay)
     return delay
