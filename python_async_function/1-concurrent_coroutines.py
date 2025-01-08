404e0edda02936833821245f5a0a5015498b87ab
Dtbissy <isaactheedwards@gmail.com>
2025-01-07 21:38:06 -0600

    Well save for tomorrow

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""Will display a asynchronous function max delay and max ns"""
+import asyncio
+from typing import *
+from basic_async_syntax import wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    for x in n:
+
