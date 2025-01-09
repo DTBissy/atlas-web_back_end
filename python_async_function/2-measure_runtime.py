d7d1f66b24fe5c9f87701c56365b630465bd1032
Dtbissy <isaactheedwards@gmail.com>
2025-01-08 19:23:31 -0600

    Lets try this. ALittle faster time

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -8,8 +8,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 def measure_time(n: int, max_delay: int) -> float:
     """This will use the time module to measure the time it takes
     my async module to run"""
-    start = time.time()
+    start = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    end = time.time()
-    total_time = end - start
-    return total_time / n
+    elapsed = time.perf_counter() - start
+    return elapsed / n
