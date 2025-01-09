a5d50a90ccfd5a86611b7407d5c275b01303b81e
Dtbissy <isaactheedwards@gmail.com>
2025-01-08 19:49:29 -0600

    I am such a dirty dog it astonishes me

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""Takes an integer for max delay and returns asynchio.Task"""
+import asyncio
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    return asyncio.create_task(asyncio.sleep(max_delay))
