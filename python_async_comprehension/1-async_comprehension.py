7a6d824dbd0764b47fc863acac582a60bf2d29cb
Dtbissy <isaactheedwards@gmail.com>
2025-01-08 21:11:39 -0600

    This is why you read the material boys. Succesfully generated numbers and stored them using async comphrension

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""This will collect 10 random numbs using
+async generator"""
+import asyncio
+ag = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension() -> int:
+    """Returns a list of random numbers retrieved from my
+    async generator function"""
+    nums = [x async for x in ag()]
+    return nums
