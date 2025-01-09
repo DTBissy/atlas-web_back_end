f24786ef8e042a0796c4b136d7796bcdbe637875
Dtbissy <isaactheedwards@gmail.com>
2025-01-08 21:00:06 -0600

    Yield at the yield sign

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""This is my async generator"""
+import asyncio
+from typing import List
+import random
+
+
+async def async_generator() -> object:
+    numbers = []
+    for x in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
