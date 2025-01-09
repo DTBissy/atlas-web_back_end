eb25be4abc6bbebd03099cf62e7c2ddbe845e385
Dtbissy <isaactheedwards@gmail.com>
2025-01-08 23:02:59 -0600

    Like this?

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -6,12 +6,7 @@ ac = __import__("1-async_comprehension").async_comprehension
 
 async def measure_runtime() -> float:
     """This will start my count timer and then start my async
-    comprehension function. This will send it into a list comprehension
-    async function that will then use my async gen function
-     to generate 10 random numbers. It will generate 10 numbers
-    10 times. I think it takes roughly ten seconds because the
-    event loop holds everything up and knocks them down
-    at the same time."""
+    comprehension function. This will send it into a list."""
     start = time.perf_counter()
     await asyncio.gather(ac(),ac(),ac(),ac())
     elapsed = time.perf_counter() - start
