f0b4d95cea6ad43455acd94f1b912e7bc6d1099b
Dtbissy <isaactheedwards@gmail.com>
2025-01-14 16:09:27 -0600

    Spoke WAAAY to soon time for a break

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""This a Last in First out
+"""This a First in First out
 class module, which inherits basecaching"""
 from base_caching import BaseCaching
 from typing import Dict
 
 
 class LIFOCache(BaseCaching):
-    """This class utilizes Last in First out
-    Algorthims to manage the cache"""
+    """This class will use First In First Out
+    algorithms to mangage the caching process"""
     def __init__(self):
         super().__init__()
+        self.order = []
 
     def put(self, key: str, item: str) -> Dict[str, str]:
         """This will update the dictionary
@@ -18,14 +19,7 @@ class LIFOCache(BaseCaching):
         if key is None or item is None:
             return None
         else:
-            add = self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                fo = list(self.cache_data.keys())[-1]
-                print(f"DISCARD: {fo}")
-                del self.cache_data[fo]
-                return self.cache_data
-            else:
-                return add
+            return self.cache_data
 
     def get(self, key: str) -> str:
         """This function prints the entered key"""
