fcf896c79a9d361cc23ead4eb0a4a35951a760d1
Dtbissy <isaactheedwards@gmail.com>
2025-01-15 17:54:04 -0600

    Cant get away with the sentences just yet

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -19,14 +19,19 @@ class LRUCache(BaseCaching):
             return value
         return -1
 
-    def put(self, key: int, value: int) -> None:
+    def put(self, key: int, item: int) -> None:
         """Updates the value in the value in the
         cache using Least Recently Used algorithims"""
-        if len(self.cache_data) == BaseCaching.MAX_ITEMS and key not in self.cache_data:
-            popped = self.cache_data.popitem(last=False)
-            print(f"DISCARD: {popped[0]}")
-            del popped  # Remove oldest item
-        elif key in self.cache_data:
-            self.cache_data.pop(key)
+        if key is None:
+            return None
+        if item is None:
+            return None
+        else:
+            if len(self.cache_data) == BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                popped = self.cache_data.popitem(last=False)
+                print(f"DISCARD: {popped[0]}")
+                del popped  # Remove oldest item
+            elif key in self.cache_data:
+                self.cache_data.pop(key)
 
-        self.cache_data[key] = value
+            self.cache_data[key] = item
