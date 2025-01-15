64175bdf037db435a7f658aa2048498d990f2c8e
Dtbissy <isaactheedwards@gmail.com>
2025-01-15 17:50:39 -0600

    Docuementation

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -1,12 +1,18 @@
+#!/usr/bin/env python3
+"""This is a module using Least Recently Used
+algorithims"""
 from collections import OrderedDict
 from base_caching import BaseCaching
 
 class LRUCache(BaseCaching):
+    """This is the Least Recently Used Algorithims
+    class."""
     def __init__(self):
         super().__init__()
         self.cache_data = OrderedDict()
 
     def get(self, key: int) -> int:
+        """Gets the value in the cache"""
         if key in self.cache_data:
             value = self.cache_data.pop(key)
             self.cache_data[key] = value  # Move to end to mark as recently used
@@ -14,6 +20,8 @@ class LRUCache(BaseCaching):
         return -1
 
     def put(self, key: int, value: int) -> None:
+        """Updates the value in the value in the
+        cache using Least Recently Used algorithims"""
         if len(self.cache_data) == BaseCaching.MAX_ITEMS and key not in self.cache_data:
             popped = self.cache_data.popitem(last=False)
             print(f"DISCARD: {popped[0]}")
