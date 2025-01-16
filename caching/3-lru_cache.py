de56be2a8e3ab5a87bcad71eac45c907b066c86e
Dtbissy <isaactheedwards@gmail.com>
2025-01-15 18:07:41 -0600

    Hold on

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -13,12 +13,11 @@ class LRUCache(BaseCaching):
 
     def get(self, key: int) -> int:
         """Gets the value in the cache"""
-        if key is None:
-            return None
-        else:
+        if key in self.cache_data:
             value = self.cache_data.pop(key)
             self.cache_data[key] = value  # Move to end to mark as recently used
             return value
+        return -1
 
     def put(self, key: int, item: int) -> None:
         """Updates the value in the value in the
